b4a7734d590c96ca5416f7275361b9eda1da47d6
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2021-07-31 16:57:48 +0300

    Create entry-point module for GraphQL DSL

---
+++ lib/graph_ql/dsl.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'dsl/version'
+
+module GraphQL
+  # GraphQL DSL entry-point
+  module DSL
+  end
+end
--- lib/graph_ql_dsl.rb
+++ lib/graph_ql_dsl.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
-require_relative 'graph_ql/dsl/version'
-
-module GraphQL
-  # GraphQL DSL entry-point
-  module DSL
-  end
-end
+require_relative 'graph_ql/dsl'
