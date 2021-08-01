3a479aafd5464526c3fdc6563951baa536e85070
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2021-08-01 20:12:55 +0300

    Add several examples to GraphQL::DSL

--- lib/graph_ql/dsl.rb
+++ lib/graph_ql/dsl.rb
@@ -22,6 +22,49 @@ module GraphQL
     # @param block [Proc] declare DSL for operations
     #
     # @return [Nodes::ExecutableDocument] executable GraphQL document
+    #
+    # @example Create executable document with several queries
+    #  executable_document = GraphQL::DSL.executable_document {
+    #    query(:sheep) {
+    #      animal(type: :sheep) {
+    #        __fragment :animal
+    #      }
+    #    }
+    #
+    #    query(:she_goats) {
+    #      animal(type: :she_goat) {
+    #        __fragment :animal
+    #      }
+    #    }
+    #
+    #    fragment(:animal, :Animal) {
+    #      name
+    #      age
+    #    }
+    #  }
+    #
+    #  puts executable_document.to_gql
+    #  # query sheep
+    #  # {
+    #  #   animal(type: "sheep")
+    #  #   {
+    #  #     ...animal
+    #  #   }
+    #  # }
+    #  #
+    #  # query she_goats
+    #  # {
+    #  #   animal(type: "she_goat")
+    #  #   {
+    #  #     ...animal
+    #  #   }
+    #  # }
+    #  #
+    #  # fragment animal on Animal
+    #  # {
+    #  #   name
+    #  #   age
+    #  # }
     def self.executable_document(&block)
       Nodes::ExecutableDocument.new(&block)
     end
@@ -33,6 +76,24 @@ module GraphQL
     # @param block [Proc] declare DSL for sub-fields
     #
     # @return [Nodes::Operation] GraphQL query
+    #
+    # @example Create query
+    #   query = GraphQL::DSL.query(:sheep) {
+    #     animal(type: :sheep) {
+    #       name
+    #       age
+    #     }
+    #   }
+    #
+    #   puts query.to_gql
+    #   # query sheep
+    #   # {
+    #   #   animal(type: "sheep")
+    #   #   {
+    #   #     name
+    #   #     age
+    #   #   }
+    #   # }
     def self.query(name = nil, &block)
       Nodes::Operation.new(:query, name, &block)
     end
@@ -44,6 +105,26 @@ module GraphQL
     # @param block [Proc] declare DSL for sub-fields
     #
     # @return [Nodes::Operation] GraphQL mutation
+    #
+    # @example Create mutation
+    #   mutation = GraphQL::DSL.mutation(:create_sheep) {
+    #     createSheep(name: 'Milly', age: 5) {
+    #       id
+    #       name
+    #       age
+    #     }
+    #   }
+    #
+    #   puts mutation.to_gql
+    #   # mutation create_sheep
+    #   # {
+    #   #   createSheep(name: "Milly", age: 5)
+    #   #   {
+    #   #     id
+    #   #     name
+    #   #     age
+    #   #   }
+    #   # }
     def self.mutation(name = nil, &block)
       Nodes::Operation.new(:mutation, name, &block)
     end
@@ -55,6 +136,26 @@ module GraphQL
     # @param block [Proc] declare DSL for sub-fields
     #
     # @return [Nodes::Operation] GraphQL subscription
+    #
+    # @example Create subscription
+    #   subscription = GraphQL::DSL.subscription(:sheep_jumps) {
+    #     animal(type: :sheep) {
+    #       id
+    #       name
+    #       age
+    #     }
+    #   }
+    #
+    #   puts subscription.to_gql
+    #   # subscription sheep_jumps
+    #   # {
+    #   #   animal(type: "sheep")
+    #   #   {
+    #   #     id
+    #   #     name
+    #   #     age
+    #   #   }
+    #   # }
     def self.subscription(name = nil, &block)
       Nodes::Operation.new(:subscription, name, &block)
     end
@@ -67,6 +168,21 @@ module GraphQL
     # @param block [Proc] declare DSL for sub-fields
     #
     # @return [Nodes::FragmentOperation] GraphQL fragment
+    #
+    # @example Create fragment
+    #   fragment = GraphQL::DSL.fragment(:animal, :Animal) {
+    #     id
+    #     name
+    #     age
+    #   }
+    #
+    #   puts fragment.to_gql
+    #   # fragment animal on Animal
+    #   # {
+    #   #   id
+    #   #   name
+    #   #   age
+    #   # }
     def self.fragment(name, type, &block)
       Nodes::FragmentOperation.new(name, type, &block)
     end
